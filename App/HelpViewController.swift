e18eb25f6f34319a54924bc58e80091c45ef7828
Your Name HereKyler <bluekylerblue@gmail.com>
2016-12-08 17:07:27 -0800

    Dance gesture education (help screen), Launch screen, App Icon, button cleanup

---
+++ App/HelpViewController.swift
@@ -0,0 +1,42 @@
+//
+//  HelpViewController.swift
+//  Dance Commander
+//
+//  Created by Kyler Blue on 12/8/16.
+//  Copyright © 2016 Dance Commander. All rights reserved.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+    
+    @IBOutlet weak var closeButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func didPressClose(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
